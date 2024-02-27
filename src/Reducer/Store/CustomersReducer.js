6c5cf91525a9bf5c82236bef29590b661e77363e
Tech Rules <66055840+techrules07@users.noreply.github.com>
2024-02-27 20:42:00 +0530

    Customers saga

---
+++ src/Reducer/Store/CustomersReducer.js
@@ -0,0 +1,21 @@
+import { GET_ALL_CUSTOMERS_API_RESPONSE, ERROR_MESSAGE } from "../../utils/Constant";
+
+const INITIAL_STATE = {
+    customersList: [],
+    error: null
+}
+
+const CustomersReducer = (state = INITIAL_STATE, action) => {
+
+    switch (action.type) {
+        case GET_ALL_CUSTOMERS_API_RESPONSE: {
+            return {...state, customersList: action.payload}
+        }
+        case ERROR_MESSAGE: {
+            return {...state, error: action.payload}
+        }
+    }
+    return state;
+}
+
+export default CustomersReducer;
