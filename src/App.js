9c7813d743d6cebe38ff3cc4e046822fbc018236
Sudharraja <ssudhar997@gmail.com>
2024-02-29 18:47:16 +0530

    UI Design has been modified a bit

--- src/App.js
+++ src/App.js
@@ -102,7 +102,7 @@ function App() {
           />
 
           <Route
-            path="/Index"
+            path="/Purchaseorder"
             element={
               <>
                 <Navbar />
