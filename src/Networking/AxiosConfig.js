961ec2ba50f1970666933ff8bda97ea3a8a9f1ef
Tech Rules <66055840+techrules07@users.noreply.github.com>
2024-03-10 13:42:58 +0530

    url issue resolved

--- src/Networking/AxiosConfig.js
+++ src/Networking/AxiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const AxiosConfig = axios.create({
-        baseURL: 'http://68.178.161.233:8080/', 
+        baseURL: 'http://68.178.161.233:8080/handt/', 
         headers: {
             "Content-Type": "application/json",
         }
